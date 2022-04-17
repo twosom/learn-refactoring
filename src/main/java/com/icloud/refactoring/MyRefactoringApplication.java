a6fbd1ca2b17a0bb6c2440f80455c21db8727bf8
hope <hope@gravylab.co.kr>
2022-04-17 18:00:11 +0900

    init project

---
+++ src/main/java/com/icloud/refactoring/MyRefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.icloud.refactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRefactoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRefactoringApplication.class, args);
+    }
+
+}
