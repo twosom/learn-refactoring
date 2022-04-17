10db4a16333d60b893ce65221303e9376900909b
hope <hope@gravylab.co.kr>
2022-04-17 18:58:27 +0900

    learn mysterious_name

---
+++ src/main/java/com/icloud/refactoring/_01_smell_mysterious_name/_01_before/StudyDashboard.java
@@ -0,0 +1,49 @@
+package com.icloud.refactoring._01_smell_mysterious_name._01_before;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class StudyDashboard {
+    private final Set<String> usernames = new HashSet<>();
+    private final Set<String> reviews = new HashSet<>();
+
+    /**
+     * 스터디 리뷰 이슈에 작성되어 있는 리뷰어 목록과 리뷰를 읽어옵니다.
+     *
+     * @param issue
+     * @throws IOException
+     */
+    private void studyReviews(GHIssue issue) throws IOException {
+        List<GHIssueComment> comments = issue.getComments();
+        for (GHIssueComment comment : comments) {
+            usernames.add(comment.getUser().getName());
+            reviews.add(comment.getBody());
+        }
+    }
+
+    public Set<String> getUsernames() {
+        return usernames;
+    }
+
+    public Set<String> getReviews() {
+        return reviews;
+    }
+
+    public static void main(String[] args) throws IOException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(30);
+
+        StudyDashboard studyDashboard = new StudyDashboard();
+        studyDashboard.studyReviews(issue);
+        studyDashboard.getUsernames().forEach(System.out::println);
+        studyDashboard.getReviews().forEach(System.out::println);
+    }
+}
---
+++ src/main/java/com/icloud/refactoring/_01_smell_mysterious_name/_04_rename_field/StudyDashboard.java
@@ -0,0 +1,44 @@
+package com.icloud.refactoring._01_smell_mysterious_name._04_rename_field;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class StudyDashboard {
+
+    private Set<StudyReview> studyReviews = new HashSet<>();
+
+    public Set<StudyReview> getStudyReviews() {
+        return studyReviews;
+    }
+
+    /**
+     * 스터디 리뷰 이슈에 작성되어 있는 리뷰어 목록과 리뷰를 읽어옵니다.
+     *
+     * @throws IOException
+     */
+    private void loadReviews() throws IOException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(30);
+
+        List<GHIssueComment> reviews = issue.getComments();
+        for (GHIssueComment review : reviews) {
+            StudyReview studyReview = new StudyReview(review.getUserName(), review.getBody());
+            this.studyReviews.add(studyReview);
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        StudyDashboard studyDashboard = new StudyDashboard();
+        studyDashboard.loadReviews();
+        studyDashboard.getStudyReviews().forEach(System.out::println);
+    }
+}
