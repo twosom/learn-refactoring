1857db265e5ffd19f83305453f0378a674d5d523
hope <hope@gravylab.co.kr>
2022-04-25 22:43:20 +0900

    merged from mutable-data refactoring

---
+++ src/main/java/com/icloud/refactoring/_06_mutable_data/_05_combine_functions_into_transform/Reading.java
@@ -0,0 +1,7 @@
+package com.icloud.refactoring._06_mutable_data._05_combine_functions_into_transform;
+
+import java.time.Month;
+import java.time.Year;
+
+public record Reading(String customer, double quantity, Month month, Year year) {
+}
---
+++ src/test/java/com/icloud/refactoring/_06_mutable_data/_04_replace_derived_variable_with_query/ProductionPlanTest.java
@@ -0,0 +1,16 @@
+package com.icloud.refactoring._06_mutable_data._04_replace_derived_variable_with_query;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductionPlanTest {
+
+    @Test
+    void production() {
+        ProductionPlan productionPlan = new ProductionPlan();
+        productionPlan.applyAdjustment(10);
+        productionPlan.applyAdjustment(20);
+        assertEquals(30, productionPlan.getProduction());
+    }
+}
