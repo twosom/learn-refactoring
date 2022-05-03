839e7055ac328bf29cfb93350b4932b46e31df3c
hope <hope@gravylab.co.kr>
2022-05-04 00:08:49 +0900

    learn lazy-elements refactoring (almost learned intellij feature)

---
+++ src/main/java/com/icloud/refactoring/_14_lazy_elements/_01_collapse_hierarchy/Reservation.java
@@ -0,0 +1,14 @@
+package com.icloud.refactoring._14_lazy_elements._01_collapse_hierarchy;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Reservation {
+
+    private LocalDateTime endDateTime;
+
+    private List<String> members;
+
+    private String owner;
+
+}
