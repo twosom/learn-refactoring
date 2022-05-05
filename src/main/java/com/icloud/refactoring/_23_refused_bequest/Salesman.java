8e6a74b618fcda0affca39344302fe62b8db01c8
hope <hope@gravylab.co.kr>
2022-05-05 16:26:41 +0900

    learn refused-bequest refactoring

---
+++ src/main/java/com/icloud/refactoring/_23_refused_bequest/Salesman.java
@@ -0,0 +1,11 @@
+package com.icloud.refactoring._23_refused_bequest;
+
+public class Salesman extends Employee {
+
+    protected Quota quota;
+
+    protected Quota getQuota() {
+        return new Quota();
+    }
+}
+
